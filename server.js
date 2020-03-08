c293c5e5adb1d055d72e28ef85a28abd47d5f98f
Marcelo Tadeu de Souza <marcelo.tadeu@castgroup.com.br>
2020-03-08 14:05:13 -0300

    ajustes

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/sisms'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sisms/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
